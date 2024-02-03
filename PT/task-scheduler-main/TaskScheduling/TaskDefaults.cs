943973144302ab32e58ba6cf2b3acc36600c5a15
PriyanshuNiranjan <134698747+Priyanshuosdbms@users.noreply.github.com>
2024-02-04 01:36:48 +0530

    Add files via upload

---
+++ PT/task-scheduler-main/TaskScheduling/TaskDefaults.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskScheduling
+{
+    /// <summary>
+    /// Represents default values related to task services
+    /// </summary>
+    public static class TaskDefaults
+    {
+        /// <summary>
+        /// The length of time, in milliseconds, before the running schedule task times out. Set null to use default value
+        /// </summary>
+        public static int? ScheduleTaskRunTimeout => null;
+    }
+}
